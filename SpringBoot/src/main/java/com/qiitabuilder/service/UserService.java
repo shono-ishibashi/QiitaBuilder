ced4cbd298ff64c220a2b177af23e8a24f324e4f
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-10-29 11:37:22 +0900

    ログイン処理で使用するクラスの作成

--- SpringBoot/src/main/java/com/qiitabuilder/service/UserService.java
+++ SpringBoot/src/main/java/com/qiitabuilder/service/UserService.java
@@ -1,10 +1,17 @@
 package com.qiitabuilder.service;
 
 import com.qiitabuilder.domain.User;
+import com.qiitabuilder.mapper.UserMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@MapperScan(basePackages = {"com.qiitabuilder.mapper"})
 public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
 
     public User findByUserId(Integer userId) {
         return null;
@@ -15,5 +22,9 @@ public class UserService {
     }
 
     public void insertUser(User user) {
+    }
+
+    public void test() {
+        System.out.println(userMapper.selectTest());
     }
 }
