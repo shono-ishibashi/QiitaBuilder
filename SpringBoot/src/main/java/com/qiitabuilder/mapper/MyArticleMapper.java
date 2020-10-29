bf1b89ffac1f34178a6f7775052a42e450944845
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-10-29 15:56:01 +0900

    Create MyArticle classes

--- SpringBoot/src/main/java/com/qiitabuilder/mapper/MyArticleMapper.java
+++ SpringBoot/src/main/java/com/qiitabuilder/mapper/MyArticleMapper.java
@@ -2,10 +2,6 @@ package com.qiitabuilder.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-import java.util.Map;
-
 @Mapper
-public interface UserMapper {
-    List<Map<String,String>> selectTest();
+public class MyArticleMapper {
 }
