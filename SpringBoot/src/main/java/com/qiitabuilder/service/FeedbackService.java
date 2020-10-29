5859b0bbf12e8636d7266b1d358460e1e280d704
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-10-29 16:40:15 +0900

    feedbackクラス作成

--- SpringBoot/src/main/java/com/qiitabuilder/service/FeedbackService.java
+++ SpringBoot/src/main/java/com/qiitabuilder/service/FeedbackService.java
@@ -1,9 +1,24 @@
 package com.qiitabuilder.service;
 
+import com.qiitabuilder.domain.Feedback;
+import com.qiitabuilder.mapper.FeedbackMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class FeedbackService {
+    @Autowired
+    private FeedbackMapper feedbackMapper;
+
+    public Feedback postFeedback(Feedback feedback){
+        feedbackMapper.insert(feedback);
+        return null;
+    }
+
+    public Feedback updateFeedback(Feedback feedback){
+        return null;
+    }
+
 }
