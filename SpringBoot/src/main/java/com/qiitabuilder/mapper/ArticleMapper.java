4a3177abcae881427a5b86af25bcb4c102fbd29d
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-02 11:16:47 +0900

    getPostedArticleCountRank()、getCountByUserId()を追加

--- SpringBoot/src/main/java/com/qiitabuilder/mapper/ArticleMapper.java
+++ SpringBoot/src/main/java/com/qiitabuilder/mapper/ArticleMapper.java
@@ -1,7 +1,14 @@
 package com.qiitabuilder.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
+@Repository
 public interface ArticleMapper {
+
+    List<Integer> getPostedArticleCountRank();
+    Integer getCountByUserId(Integer userId);
 }
