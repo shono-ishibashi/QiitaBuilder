8e28de6436dbaaa1a9a92fe5c27668771e1bfeaf
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-02 11:26:50 +0900

    getQiitaRecommendedRank(), getAllCountByPostedUserId(Integer postedUserId), getMostRecommendedArticleId(Integer userId, Integer sortId)を追加

--- SpringBoot/src/main/java/com/qiitabuilder/mapper/RecommendMapper.java
+++ SpringBoot/src/main/java/com/qiitabuilder/mapper/RecommendMapper.java
@@ -1,7 +1,15 @@
 package com.qiitabuilder.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
+@Repository
 public interface RecommendMapper {
+
+    List<Integer> getQiitaRecommendedRank();
+    Integer getAllCountByPostedUserId(Integer postedUserId);
+    List<Integer> getMostRecommendedArticleId(Integer userId, Integer sortId);
 }
