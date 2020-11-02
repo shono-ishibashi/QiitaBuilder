6dc06d9e5da771ce2368442fed5948b3a0175373
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-02 16:34:34 +0900

    記事詳細を取得時の例外処理を実装

--- SpringBoot/src/main/java/com/qiitabuilder/controller/ArticleController.java
+++ SpringBoot/src/main/java/com/qiitabuilder/controller/ArticleController.java
@@ -1,9 +1,9 @@
 package com.qiitabuilder.controller;
 
 import com.qiitabuilder.domain.Article;
+import com.qiitabuilder.exception.BadRequestException;
+import com.qiitabuilder.exception.NotFoundException;
 import com.qiitabuilder.form.SearchArticleForm;
-import com.qiitabuilder.service.ArticleService;
-import com.qiitabuilder.mapper.TagMapper;
 import com.qiitabuilder.service.ArticleService;
 import com.qiitabuilder.service.QiitaAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 
 @RestController
@@ -41,8 +41,24 @@ public class ArticleController {
 
     @GetMapping("/{articleId}")
     @ResponseStatus(HttpStatus.OK)
-    public Article getArticle(@PathVariable("articleId") Integer articleId) {
-        return articleService.getArticle(articleId);
+    public Article getArticle(@PathVariable("articleId") String strArticleId) {
+        Integer articleId;
+        // 入力値が正しくない場合はBadRequestExceptionを投げる
+        try {
+            articleId = Integer.parseInt(strArticleId);
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("");
+        }
+
+        Article result = articleService.getArticle(articleId);
+
+        // 検索結果がない場合はNotFoundExceptionを投げる
+        if (Objects.isNull(result)) {
+            throw new NotFoundException("not found");
+        }
+
+        return result;
+
     }
 
     /////////////////////////////
