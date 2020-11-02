697730b29daff033eccdaf496ffc2f21b7488334
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-02 11:05:30 +0900

    エンドポイントPOST/recommendを実装

--- SpringBoot/src/main/java/com/qiitabuilder/controller/RecommendController.java
+++ SpringBoot/src/main/java/com/qiitabuilder/controller/RecommendController.java
@@ -1,11 +1,17 @@
 package com.qiitabuilder.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.qiitabuilder.domain.Recommend;
+import com.qiitabuilder.service.RecommendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/my-article")
+@RequestMapping(value = "/recommend")
 public class RecommendController {
+
+    @Autowired
+    private RecommendService recommendService;
 
     /////////////////////////////
     //// GET
@@ -14,6 +20,11 @@ public class RecommendController {
     /////////////////////////////
     //// POST
     /////////////////////////////
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public Recommend postRecommend(@RequestBody Recommend recommend) {
+        return recommendService.postRecommend(recommend);
+    }
 
     /////////////////////////////
     //// PUT
--- SpringBoot/src/main/java/com/qiitabuilder/mapper/RecommendMapper.java
+++ SpringBoot/src/main/java/com/qiitabuilder/mapper/RecommendMapper.java
@@ -1,7 +1,21 @@
 package com.qiitabuilder.mapper;
 
+import com.qiitabuilder.domain.Recommend;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RecommendMapper {
+
+    /**
+     * Recommendを新規作成する
+     * @param recommend
+     */
+    void insert(Recommend recommend);
+
+    /**
+     * 自動採番されたプライマリーキーの値を取得する
+     * @return 自動採番の値
+     */
+    Integer getAutoIncrementKey();
+
 }
