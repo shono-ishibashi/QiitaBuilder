c01a0929ad6d28a26f4253972f004ca897a8b46a
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-04 16:33:14 +0900

    QiitaAPI Oauth認証の実装

--- SpringBoot/src/main/java/com/qiitabuilder/mapper/UserMapper.java
+++ SpringBoot/src/main/java/com/qiitabuilder/mapper/UserMapper.java
@@ -7,12 +7,20 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @Mapper
 @Repository
 public interface UserMapper {
     List<Map<String,String>> selectTest();
+
     Optional<User> findByUid(String uid);
+
     Optional<User> findByUserId(Integer userId);
+
     void insert(User user);
+
+    String findQiitaAPICodeByUserId(Integer userId);
+
+    String updateQiitaAPICode(String qiitaAPICode,Integer userId);
 }
--- SpringBoot/src/main/java/com/qiitabuilder/security/SecurityConfig.java
+++ SpringBoot/src/main/java/com/qiitabuilder/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 // AUTHORIZE
                 .authorizeRequests()
-                .mvcMatchers("/","/article/*")
+                .mvcMatchers("/","/article/*","/qiita/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
