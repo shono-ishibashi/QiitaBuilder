1894026c462e29d7b0bfdf0c07fbf6c40584922f
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-11-04 11:37:12 +0900

    vuetify,axiosをpluginしました modulesの雛形を作成しました

--- Vue/src/store/index.js
+++ Vue/src/store/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './modules/auth'
+import articles from './modules/articles'
+import article from './modules/article'
+import users from './modules/users'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
@@ -11,5 +16,10 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    auth:auth,
+    articles:articles,
+    article:article,
+    users:users,
+    user:user
   }
 })
