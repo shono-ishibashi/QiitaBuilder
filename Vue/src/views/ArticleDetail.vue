cb8ed99e263cc6c63dfe05966ee2e2a0a748622f
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-04 15:04:46 +0900

    記事詳細画面のVueファイル作成

---
+++ Vue/src/views/ArticleDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Article Detail
+    <article/>
+    <feedback/>
+    <feedbackEditor/>
+  </div>
+</template>
+
+<script>
+import article from '../components/Article'
+import feedback from '../components/Feedback'
+import feedbackEditor from '../components/FeedbackEditor'
+export default {
+  components:{
+    article,
+    feedback,
+    feedbackEditor
+  }
+}
+</script>
