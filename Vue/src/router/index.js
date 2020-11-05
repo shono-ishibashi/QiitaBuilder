32cec943270c1c592131d4fda71720fa2faac8b3
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-05 14:15:13 +0900

    vue router - firebase auth の連携

--- Vue/src/router/index.js
+++ Vue/src/router/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ArticleList from '../views/ArticleList'
+import firebase from "firebase";
+import store from "../store/index";
+import axios from "axios";
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:'/articleList',
+    name:'ArticleList',
+    component:ArticleList
+  },
   {
     path: '/article/detail',
     name: 'articleDetail',
@@ -13,6 +22,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/qiitatest',
+    name: 'qiitatest',
+    component: () => import(/* webpackChunkName: "login" */ '../views/QiitaTest.vue')
   }
 ]
 
@@ -20,6 +34,40 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+const API_URL = 'http://localhost:8080/qiita_builder/';
+
+router.beforeResolve((to, from, next) => {
+  if (to.path === '/login') {
+    next()
+  } else {
+    //ログイン中の処理
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        store.commit('setLoginUser',firebase.auth().currentUser);
+        const uid = firebase.auth().currentUser.uid;
+        firebase.firestore().collection("users").doc(uid).get()
+            .then(data => {
+              axios.post( API_URL + 'login?uid=' + uid + '&password=' + data.data().password,{},{
+              })
+                  .then(response => {
+                    //Vuexにjwt Tokenを追加
+                    store.commit("auth/setAPIToken", response.headers.authorization);
+                  })
+            })
+        if (to.path === '/login') {
+          next( {path : '/'});
+        } else {
+          next();
+        }
+      } else {
+        store.commit('setAPIToken',null);
+        store.dispatch('auth/logout');
+        next({path: '/login'})
+      }
+    })
+  }
 })
 
 export default router
