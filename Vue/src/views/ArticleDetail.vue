4109c1b09f7a63d961fd2d7187de413029ecd36f
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-05 17:40:30 +0900

    追従ボタンの作成

--- Vue/src/views/ArticleDetail.vue
+++ Vue/src/views/ArticleDetail.vue
@@ -1,10 +1,23 @@
 <template>
-  <v-container>
-    Article Detail
-    <Article :article="article"/>
-    <Feedback :feedbacks="feedbacks"/>
+  <v-app>
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-btn elevation="2" id="qiita_btn">Qiita</v-btn>
+            <v-btn elevation="2" id="my_btn">My記事登録</v-btn>
+          </v-col>
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <Article :article="article" />
+              <Feedback :feedbacks="feedbacks" />
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
     <FeedbackEditor />
-  </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -26,9 +39,9 @@ export default {
     article() {
       return this.$store.state.article.article;
     },
-    feedbacks(){
+    feedbacks() {
       return this.$store.state.article.article.feedbacks;
-    }
+    },
   },
   created() {
     this.fetchArticle(this.slug);
@@ -38,3 +51,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#qiita_btn {
+  position: sticky;
+  top: 35%;
+}
+#my_btn {
+  position: sticky;
+  top: 45%;
+}
+</style>
