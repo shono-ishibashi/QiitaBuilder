f2af54b39b3f87778f89d768a8ef574bf00cf964
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-11-05 17:06:47 +0900

    ユーザー詳細画面：記事表示部分作成

--- Vue/src/views/UserDetail.vue
+++ Vue/src/views/UserDetail.vue
@@ -1,58 +1,65 @@
 <template>
   <v-container>
-      <v-row style="height: 150px;" justify="center">
-        <v-col cols=5>{{ userDetail.photoUrl }}</v-col>
-        <v-col cols=5>
-            <v-data-table
-                :headers="tagsHeaders"
-                :items="userDetail.usedTags"
-                style="background-color: lightgray"
-                hide-default-footer
-                disable-pagination></v-data-table>
-        </v-col>
+    <v-row style="height: 150px;" justify="center">
+      <v-col cols=5>{{ userDetail.photoUrl }}</v-col>
+      <v-col cols=5>
+        <v-data-table
+            :headers="tagsHeaders"
+            :items="userDetail.usedTags"
+            style="background-color: lightgray"
+            hide-default-footer
+            disable-pagination></v-data-table>
+      </v-col>
 
-        <v-col cols=5>{{ userDetail.displayName }}</v-col>
-        <v-col cols=4>
-          <v-btn color="secondary" elevation="2">Qiita連携</v-btn>
-        </v-col>
+      <v-col cols=5>{{ userDetail.displayName }}</v-col>
+      <v-col cols=4>
+        <v-btn color="secondary" elevation="2">Qiita連携</v-btn>
+      </v-col>
 
-        <v-col cols=4>
-          <v-card>
-            <v-layout justify-center>
-              Qiita投稿数 / Builder投稿数<br></v-layout>
-            <v-layout justify-center>
-              {{ userDetail.postedArticleCount }} / {{ postedArticles.length }}
-            </v-layout>
-          </v-card>
-        </v-col>
-        <v-col cols=4>
-          <v-card>
-            <v-layout justify-center>FB数</v-layout>
-            <v-layout justify-center>
-              {{ userDetail.feedbackCount }}
-            </v-layout>
-          </v-card>
-        </v-col>
-        <v-col cols=4>
-          <v-card>
-            <v-layout justify-center>総獲得推奨数</v-layout>
-            <v-layout justify-center>
-              {{ userDetail.qiitaRecommendedAllCount }}
-            </v-layout>
-          </v-card>
-        </v-col>
+      <v-col cols=4>
+        <v-card>
+          <v-layout justify-center>
+            Qiita投稿数 / Builder投稿数<br></v-layout>
+          <v-layout justify-center>
+            {{ userDetail.postedArticleCount }} / {{ postedArticles.length }}
+          </v-layout>
+        </v-card>
+      </v-col>
+      <v-col cols=4>
+        <v-card>
+          <v-layout justify-center>FB数</v-layout>
+          <v-layout justify-center>
+            {{ userDetail.feedbackCount }}
+          </v-layout>
+        </v-card>
+      </v-col>
+      <v-col cols=4>
+        <v-card>
+          <v-layout justify-center>総獲得推奨数</v-layout>
+          <v-layout justify-center>
+            {{ userDetail.qiitaRecommendedAllCount }}
+          </v-layout>
+        </v-card>
+      </v-col>
 
-        <v-col cols=2>
-          <v-btn color="secondary">投稿記事</v-btn>
-        </v-col>
-        <v-col cols=2>
-          <v-btn color="secondary">FB記事</v-btn>
-        </v-col>
-        <v-col cols=2>
-          <v-btn color="secondary">My記事</v-btn>
-        </v-col><v-col cols="6"></v-col>
+      <v-col cols="2">
+        <v-btn color="secondary">投稿記事</v-btn>
+      </v-col>
+      <v-col cols="2">
+        <v-btn color="secondary">FB記事</v-btn>
+      </v-col>
+      <v-col cols="2">
+        <v-btn color="secondary">My記事</v-btn>
+      </v-col>
+      <v-col cols="6"></v-col>
 
-      </v-row>
+      <v-col cols="10">
+        <v-card>
+
+        </v-card>
+      </v-col>
+
+    </v-row>
 
   </v-container>
 </template>
@@ -69,6 +76,7 @@ export default {
         {text: "タグ名", value: "tagName"},
         {text: "使用回数", value: "usedTagCount"}
       ],
+      articles:[],
     };
   },
   computed: {
@@ -82,6 +90,7 @@ export default {
     this.fetchPostedArticles(this.$route.query.userId);
     this.fetchFeedbackArticles(this.$route.query.userId);
     this.fetchMyArticles(this.$route.query.userId);
+    this.articles=this.postedArticles;
   }
 }
 </script>
