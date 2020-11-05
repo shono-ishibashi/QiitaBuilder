9aa2eb4775ab6f33d9e6a983ef264f6fb020c13d
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-05 12:27:29 +0900

    firebase auth と RESTfulAPIの連携の実装

--- Vue/src/store/modules/auth.js
+++ Vue/src/store/modules/auth.js
@@ -1,9 +1,76 @@
+import firebase from 'firebase';
+import {v4 as uuidv4} from 'uuid';
+import axios from 'axios'
+import router from "@/router";
+
+const API_URL = 'http://localhost:8080/qiita_builder/';
+
 export default {
-    namespaced:true,
+    namespaced: true,
     state: {
+        loginUser: null,
+        apiToken: ''
     },
     mutations: {
+        setLoginUser(state, loginUser) {
+            state.loginUser = loginUser;
+        },
+        setAPIToken(state, apiToken) {
+            state.apiToken = apiToken;
+        }
+
     },
     actions: {
+        googleLogin({commit}) {
+            //firebase google authログイン
+            const provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(() => {
+                    const loginUser = firebase.auth().currentUser;
+                    const db = firebase.firestore();
+
+                    commit('setLoginUser', loginUser);
+
+                    db.collection('users').doc(loginUser.uid).get()
+                        .then(data => {
+                            //firestore にユーザーのデータが存在するのかを確認
+                            //--あれば、そのデータを用いてRESTAPIにログイン処理を行う
+                            //--なければ、ユーザーデータをfirestoreに追加して、RESTAPIにも追加する
+
+                            if (!data.exists) {
+                                const password = uuidv4();
+                                db.collection('users').doc(loginUser.uid)
+                                    .set({password: password}).then(() => {
+                                    axios.post(API_URL, {
+                                        uid: loginUser.uid,
+                                        password: password,
+                                        photoUrl: loginUser.photoURL,
+                                        displayName: loginUser.displayName
+                                    }).then((result) => {
+                                        console.log(result);
+                                    })
+                                })
+                            }
+                        })
+                db.collection('users').doc(loginUser.uid).get()
+                    .then(data => {
+                        const request = API_URL + 'login?uid=' + loginUser.uid + '&password=' + data.data().password;
+                        axios.post(request,{},{headers:{'Content-Type': 'application/json'}})
+                            .then(response => {
+                                //Vuexにheaderを追加
+                                commit("setAPIToken", response.headers.authorization);
+                                router.push('/')
+                            })
+                    })
+                },
+            )
+        },
+        logout({commit}) {
+            firebase.auth().signOut().then(function () {
+                commit('setLoginUser', null);
+                axios.post(API_URL + 'logout').then(() => {
+                    router.push('/login')
+                })
+            })
+        }
     }
 }
