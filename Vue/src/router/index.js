3fdd7f5c9e5be585e6a6f007bddb375146a2b515
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-05 12:27:42 +0900

    firebase auth と RESTfulAPIの連携の実装

--- Vue/src/router/index.js
+++ Vue/src/router/index.js
@@ -4,11 +4,16 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  {	
-    path: '/article/detail',	
-    name: 'articleDetail',	
+  {
+    path: '/article/detail',
+    name: 'articleDetail',
     component: () => import(/* webpackChunkName: "articleDetail" */ '../views/ArticleDetail.vue')
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  }
 ]
 
 const router = new VueRouter({
