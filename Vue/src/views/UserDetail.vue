29957adbedc7caf92689a31385b21505922aae0f
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-11-05 16:24:22 +0900

    ユーザー詳細画面：ユーザー詳細情報表示部分作成

--- Vue/src/views/UserDetail.vue
+++ Vue/src/views/UserDetail.vue
@@ -1,61 +1,59 @@
 <template>
   <v-container>
-    <v-row style="height: 150px;" justify="center">
-      <v-col cols=5>{{ userDetail.photoUrl }}</v-col>
-      <v-col cols=5>
-        <v-row>
-          <v-data-table
-              :headers="tagsHeaders"
-              :items="userDetail.usedTags"
-              style="background-color: lightgray"
-              hide-default-footer
-              disable-pagination></v-data-table>
-        </v-row>
-      </v-col>
-    </v-row>
+      <v-row style="height: 150px;" justify="center">
+        <v-col cols=5>{{ userDetail.photoUrl }}</v-col>
+        <v-col cols=5>
+            <v-data-table
+                :headers="tagsHeaders"
+                :items="userDetail.usedTags"
+                style="background-color: lightgray"
+                hide-default-footer
+                disable-pagination></v-data-table>
+        </v-col>
 
-    <v-row style="height: 50px;" justify="center">
-      <v-col cols=5>{{ userDetail.displayName }}</v-col>
-      <v-col cols=5>
-        <v-btn color="secondary" elevation="2">Qiita連携</v-btn>
-      </v-col>
-    </v-row>
+        <v-col cols=5>{{ userDetail.displayName }}</v-col>
+        <v-col cols=4>
+          <v-btn color="secondary" elevation="2">Qiita連携</v-btn>
+        </v-col>
 
-    <v-row
-        style="height: 20px;"
-        justify="center">
-      <v-col cols=4>
-        <v-card>
-          <v-layout justify-center>
-            Qiita投稿数 / Builder投稿数<br></v-layout>
-          <v-layout justify-center>
-            {{ userDetail.postedArticleCount }} / {{ postedArticles.length }}
-          </v-layout>
-        </v-card>
-      </v-col>
-      <v-col cols=4>
-        <v-card>
-          <v-layout justify-center>FB数</v-layout>
-          <v-layout justify-center>
-            {{ userDetail.feedbackCount }}
-          </v-layout>
-        </v-card>
-      </v-col>
-      <v-col cols=4>
-        <v-card>
-          <v-layout justify-center>総獲得推奨数</v-layout>
-          <v-layout justify-center>
-            {{ userDetail.qiitaRecommendedAllCount }}
-          </v-layout>
-        </v-card>
-      </v-col>
-    </v-row>
+        <v-col cols=4>
+          <v-card>
+            <v-layout justify-center>
+              Qiita投稿数 / Builder投稿数<br></v-layout>
+            <v-layout justify-center>
+              {{ userDetail.postedArticleCount }} / {{ postedArticles.length }}
+            </v-layout>
+          </v-card>
+        </v-col>
+        <v-col cols=4>
+          <v-card>
+            <v-layout justify-center>FB数</v-layout>
+            <v-layout justify-center>
+              {{ userDetail.feedbackCount }}
+            </v-layout>
+          </v-card>
+        </v-col>
+        <v-col cols=4>
+          <v-card>
+            <v-layout justify-center>総獲得推奨数</v-layout>
+            <v-layout justify-center>
+              {{ userDetail.qiitaRecommendedAllCount }}
+            </v-layout>
+          </v-card>
+        </v-col>
 
-    <v-row>
-      <v-col cols=2>
-        <v-btn color="secondary">投稿記事</v-btn>
-      </v-col>
-    </v-row>
+        <v-col cols=2>
+          <v-btn color="secondary">投稿記事</v-btn>
+        </v-col>
+        <v-col cols=2>
+          <v-btn color="secondary">FB記事</v-btn>
+        </v-col>
+        <v-col cols=2>
+          <v-btn color="secondary">My記事</v-btn>
+        </v-col><v-col cols="6"></v-col>
+
+      </v-row>
+
   </v-container>
 </template>
 
