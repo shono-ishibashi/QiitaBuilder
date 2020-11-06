9d7906fd3d1b7eef609fd0d2d62a9e32f4da7b82
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-06 18:15:52 +0900

    Chart.vueの作成

---
+++ Vue/src/components/ranking/Chart.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="area">
+    Chart
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Chart"
+}
+</script>
+
+<style scoped>
+.area {
+  background-color: hotpink;
+}
+</style>
