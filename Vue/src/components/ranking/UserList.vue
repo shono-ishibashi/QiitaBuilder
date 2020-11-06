bec966c4ee1035ce205808e38330d1e31d2fc70a
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-06 18:18:12 +0900

    UserList.vueの作成

---
+++ Vue/src/components/ranking/UserList.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="area">
+    UserList
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "UserList"
+}
+</script>
+
+<style scoped>
+.area {
+  background-color: plum;
+}
+</style>
