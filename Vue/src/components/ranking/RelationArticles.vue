abc8692ae71589e655a0c52621fe720c821645d7
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-06 18:17:56 +0900

    RelationArticles.vueの作成

---
+++ Vue/src/components/ranking/RelationArticles.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="area">
+    RelationArticles
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "RelationArticles"
+}
+</script>
+
+<style scoped>
+.area {
+  background-color: lightpink;
+}
+</style>
