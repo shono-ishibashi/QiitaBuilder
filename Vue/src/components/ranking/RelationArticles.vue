e1a0acf2ff4b1283ee139fae0342d09d96372fa7
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-10 16:49:17 +0900

    RelationArticles.vueを作成

---
+++ Vue/src/components/ranking/RelationArticles.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <h3>
+      <v-icon color="#5bc8ac">
+        mdi-book-open-blank-variant
+      </v-icon>
+      関連記事一覧
+    </h3>
+    <ArticleCard v-for="(relArticle, index) in relArticles" :key="relArticle.articleId" :article="relArticle"
+                 :index="index"></ArticleCard>
+  </v-container>
+</template>
+
+<script>
+import ArticleCard from "@/components/ArticleCard";
+
+export default {
+  name: "RelationArticles",
+  components: {
+    ArticleCard
+  },
+  props: {
+    relArticles: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
