3d2ecdbe1039ea2b2011cfd9746347c90946c349
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-10 16:41:16 +0900

    Chart.vueの作成

---
+++ Vue/src/components/ranking/Chart.vue
@@ -0,0 +1,12 @@
+<script>
+//import {Bar} from 'vue-chartjs';
+
+export default {
+  name: "Chart",
+  extends: 'importしたやつ(Bar)',
+  props: ['chartData', 'options'],
+  // mounted() {
+  //   this.renderChart(this.chartData, this.options)
+  // }
+}
+</script>
