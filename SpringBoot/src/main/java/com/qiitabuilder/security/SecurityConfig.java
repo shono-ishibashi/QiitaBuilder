14e4e2db3f0fb44aee74c7e514f9927693a43db4
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-11-10 16:43:56 +0900

    ユーザー詳細画面：fix conflict

--- SpringBoot/src/main/java/com/qiitabuilder/security/SecurityConfig.java
+++ SpringBoot/src/main/java/com/qiitabuilder/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 // AUTHORIZE
                 .authorizeRequests()
-                .mvcMatchers("/","/article/*","/qiita/*","/tag","/user/detail")
+                .mvcMatchers("/","/article/*","/qiita/*","/tag")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
