780ef76f751a1fc24098ce6b1d8b5120735473bc
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-12 17:05:37 +0900

    Qiita連携状態を確認

--- Vue/src/store/modules/auth.js
+++ Vue/src/store/modules/auth.js
@@ -4,132 +4,183 @@ import axios from 'axios'
 import router from "@/router";
 
 export default {
-    namespaced: true,
-    state: {
-        loginUser: null,
-        apiToken: ''
+  namespaced: true,
+  state: {
+    loginUser: null,
+    apiToken: "",
+    isLinkedToQiita: null,
+  },
+  mutations: {
+    setLoginUser(state, loginUser) {
+      state.loginUser = loginUser;
     },
-    mutations: {
-        setLoginUser(state, loginUser) {
-            state.loginUser = loginUser;
-        },
-        setAPIToken(state, apiToken) {
-            state.apiToken = apiToken;
-        }
-
+    setAPIToken(state, apiToken) {
+      state.apiToken = apiToken;
     },
-    actions: {
-        async googleLoginPartners({commit, rootGetters}) {
-            //firebase google authログイン
-            const provider = await new firebase.auth.GoogleAuthProvider();
-            provider.setCustomParameters({
-                hd: 'rakus-partners.co.jp'
+    setIsLinkedToQiita(state, isLinked) {
+      state.isLinkedToQiita = isLinked;
+    },
+  },
+  actions: {
+    async googleLoginPartners({ commit, rootGetters }) {
+      //firebase google authログイン
+      const provider = await new firebase.auth.GoogleAuthProvider();
+      provider.setCustomParameters({
+        hd: "rakus-partners.co.jp",
+      });
+      await firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(async () => {
+          const loginUser = await firebase.auth().currentUser;
+          const db = await firebase.firestore();
+
+          await commit("setLoginUser", loginUser);
+
+          await db
+            .collection("users")
+            .doc(loginUser.uid)
+            .get()
+            .then(async (data) => {
+              //firestore にユーザーのデータが存在するのかを確認
+              //--あれば、そのデータを用いてRESTAPIにログイン処理を行う
+              //--なければ、ユーザーデータをfirestoreに追加して、RESTAPIにも追加する
+
+              if (!data.exists) {
+                let password = await uuidv4();
+
+                await db
+                  .collection("users")
+                  .doc(loginUser.uid)
+                  .set({ password: password })
+                  .then(() => {
+                    axios
+                      .post(rootGetters.API_URL, {
+                        uid: loginUser.uid,
+                        password: password,
+                        photoUrl: loginUser.photoURL,
+                        displayName: loginUser.displayName,
+                      })
+                      .then((result) => {
+                        console.log(result);
+                      });
+                  });
+              }
             });
-            await firebase.auth().signInWithPopup(provider).then(async () => {
-                    const loginUser = await firebase.auth().currentUser;
-                    const db = await firebase.firestore();
+        });
+    },
+    async googleLoginRakus({ commit, rootGetters }) {
+      //firebase google authログイン
+      const provider = await new firebase.auth.GoogleAuthProvider();
+      provider.setCustomParameters({
+        hd: "rakus.co.jp",
+      });
+      await firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(async () => {
+          const loginUser = await firebase.auth().currentUser;
+          const db = await firebase.firestore();
 
-                    await commit('setLoginUser', loginUser);
+          await commit("setLoginUser", loginUser);
 
+          await db
+            .collection("users")
+            .doc(loginUser.uid)
+            .get()
+            .then(async (data) => {
+              //firestore にユーザーのデータが存在するのかを確認
+              //--あれば、そのデータを用いてRESTAPIにログイン処理を行う
+              //--なければ、ユーザーデータをfirestoreに追加して、RESTAPIにも追加する
 
-                    await db.collection('users').doc(loginUser.uid).get()
-                        .then(async data => {
-                            //firestore にユーザーのデータが存在するのかを確認
-                            //--あれば、そのデータを用いてRESTAPIにログイン処理を行う
-                            //--なければ、ユーザーデータをfirestoreに追加して、RESTAPIにも追加する
+              if (!data.exists) {
+                let password = await uuidv4();
 
-                            if (!data.exists) {
-                                let password = await uuidv4();
-
-                                await db.collection('users').doc(loginUser.uid)
-                                    .set({password: password}).then(() => {
-                                    axios.post(rootGetters.API_URL, {
-                                        uid: loginUser.uid,
-                                        password: password,
-                                        photoUrl: loginUser.photoURL,
-                                        displayName: loginUser.displayName
-                                    }).then((result) => {
-                                        console.log(result);
-                                    })
-                                })
-                            }
-                        })
-                },
-            )
-        },
-        async googleLoginRakus({commit, rootGetters}) {
-            //firebase google authログイン
-            const provider = await new firebase.auth.GoogleAuthProvider();
-            provider.setCustomParameters({
-                hd: 'rakus.co.jp'
+                await db
+                  .collection("users")
+                  .doc(loginUser.uid)
+                  .set({ password: password })
+                  .then(() => {
+                    axios
+                      .post(rootGetters.API_URL, {
+                        uid: loginUser.uid,
+                        password: password,
+                        photoUrl: loginUser.photoURL,
+                        displayName: loginUser.displayName,
+                      })
+                      .then((result) => {
+                        console.log(result);
+                      });
+                  });
+              }
             });
-            await firebase.auth().signInWithPopup(provider).then(async () => {
-                    const loginUser = await firebase.auth().currentUser;
-                    const db = await firebase.firestore();
-
-                    await commit('setLoginUser', loginUser);
-
-
-                    await db.collection('users').doc(loginUser.uid).get()
-                        .then(async data => {
-                            //firestore にユーザーのデータが存在するのかを確認
-                            //--あれば、そのデータを用いてRESTAPIにログイン処理を行う
-                            //--なければ、ユーザーデータをfirestoreに追加して、RESTAPIにも追加する
-
-                            if (!data.exists) {
-                                let password = await uuidv4();
-
-                                await db.collection('users').doc(loginUser.uid)
-                                    .set({password: password}).then(() => {
-                                        axios.post(rootGetters.API_URL, {
-                                            uid: loginUser.uid,
-                                            password: password,
-                                            photoUrl: loginUser.photoURL,
-                                            displayName: loginUser.displayName
-                                        }).then((result) => {
-                                            console.log(result);
-                                        })
-                                    })
-                            }
-                        })
-                },
+        });
+    },
+    async loginRESTAPI({ commit, rootGetters }, loginUser) {
+      //RESTAPI ログイン
+      console.log("loginRESTAPI");
+      const db = await firebase.firestore();
+      await db
+        .collection("users")
+        .doc(loginUser.uid)
+        .get()
+        .then(async (data) => {
+          await console.log(data);
+          await console.log(data.data());
+          const request =
+            rootGetters.API_URL +
+            "login?uid=" +
+            (await loginUser.uid) +
+            "&password=" +
+            (await data.data().password);
+          await console.log(data.data());
+          await axios
+            .post(
+              request,
+              {},
+              { headers: { "Content-Type": "application/json" } }
             )
-        },
-        async loginRESTAPI({commit, rootGetters}, loginUser) {
-            //RESTAPI ログイン
-            console.log('loginRESTAPI');
-            const db = await firebase.firestore();
-            await db.collection('users').doc(loginUser.uid).get()
-                .then(async data => {
-                    await console.log(data);
-                    await console.log(data.data());
-                    const request = rootGetters.API_URL + 'login?uid=' + await loginUser.uid + '&password=' + await data.data().password;
-                    await console.log(data.data());
-                    await axios.post(request, {}, {headers: {'Content-Type': 'application/json'}})
-                        .then(response => {
-                            //Vuexにjwt tokenを追加
-                            commit("setAPIToken", response.headers.authorization);
-                        }).catch(error =>
-                            console.log(error.status)
-                        )
-                })
-        },
-        logout({commit, rootGetters}) {
-            firebase.auth().signOut().then(function () {
-                commit('setLoginUser', null);
-                commit('setAPIToken', null);
-                axios.post(rootGetters.API_URL + 'logout').then(() => {
-                    router.push('/login')
-                })
+            .then((response) => {
+              //Vuexにjwt tokenを追加
+              commit("setAPIToken", response.headers.authorization);
             })
-        }
+            .catch((error) => console.log(error.status));
+        });
     },
-    getters: {
-        apiToken(state) {
-            return state.apiToken;
+    async checkIsLinkedToQiita({ commit, getters, rootGetters }) {
+      const url = rootGetters.API_URL + "qiita/is-linked-to-qiita";
+      const apiToken = getters.apiToken;
+      const requestConfig = {
+        headers: {
+          Authorization: apiToken,
         },
-        loginUser(state) {
-            return state.loginUser;
-        }
-    }
-}
+      };
+      try {
+        const res = await axios.get(url, requestConfig);
+        commit("setIsLinkedToQiita", res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    logout({ commit, rootGetters }) {
+      firebase
+        .auth()
+        .signOut()
+        .then(function() {
+          commit("setLoginUser", null);
+          commit("setAPIToken", null);
+          axios.post(rootGetters.API_URL + "logout").then(() => {
+            router.push("/login");
+          });
+        });
+    },
+  },
+  getters: {
+    apiToken(state) {
+      return state.apiToken;
+    },
+    loginUser(state) {
+      return state.loginUser;
+    },
+  },
+};
