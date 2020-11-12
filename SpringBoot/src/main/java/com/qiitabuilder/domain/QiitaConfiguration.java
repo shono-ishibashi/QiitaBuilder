c54b955d24f58b8ff4ffa3e7687ec66710e40985
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-12 16:20:07 +0900

    javadoc追加

--- SpringBoot/src/main/java/com/qiitabuilder/domain/QiitaConfiguration.java
+++ SpringBoot/src/main/java/com/qiitabuilder/domain/QiitaConfiguration.java
@@ -8,8 +8,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class QiitaConfiguration {
+    /**
+     * Qiita設定ID
+     */
     private Integer qiitaConfigurationId;
+
+    /**
+     * DB UserId
+     */
     private Integer userId;
+
+    /**
+     * OAuth認証時の本人確認UID
+     */
     private String state;
+
+    /**
+     * Qiita API連携用コード
+     */
     private String code;
 }
