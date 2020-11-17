efa6ebd46c80b0806ed6dd8cbc343d6583a535ec
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-17 15:39:15 +0900

    deleteメソッド作成

--- SpringBoot/src/test/java/com/qiitabuilder/mapper/MyArticleMapperTest.java
+++ SpringBoot/src/test/java/com/qiitabuilder/mapper/MyArticleMapperTest.java
@@ -235,7 +235,29 @@ class MyArticleMapperTest {
         assertEquals(2, actualResult2);
     }
 
+    //// delete()
     @Test
-    void delete() {
+    void deleteのテスト正常系() {
+        // insert
+        jdbcTemplate.execute("INSERT INTO users() VALUES();"); // Foreign key
+        jdbcTemplate.execute("INSERT INTO articles(user_id) VALUES(1);"); // Foreign key 記事1
+        jdbcTemplate.execute("INSERT INTO articles(user_id) VALUES(1);"); // Foreign key 記事2
+
+        jdbcTemplate.execute("INSERT INTO my_articles(article_id, posted_user_id, register_user_id) VALUES(1, 1, 1)");
+
+        boolean actualResult = myArticleMapper.delete(1);
+        assertTrue(actualResult);
+    }
+    @Test
+    void deleteのテスト異常系_MyArticleが存在しない場合() {
+        // insert
+        jdbcTemplate.execute("INSERT INTO users() VALUES();"); // Foreign key
+        jdbcTemplate.execute("INSERT INTO articles(user_id) VALUES(1);"); // Foreign key 記事1
+        jdbcTemplate.execute("INSERT INTO articles(user_id) VALUES(1);"); // Foreign key 記事2
+
+        jdbcTemplate.execute("INSERT INTO my_articles(article_id, posted_user_id, register_user_id) VALUES(1, 1, 1)");
+
+        boolean actualResult = myArticleMapper.delete(2);
+        assertFalse(actualResult);
     }
 }
