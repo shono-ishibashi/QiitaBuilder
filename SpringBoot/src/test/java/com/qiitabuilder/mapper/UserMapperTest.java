816a638ac37655e8aacee34f8166dc0e4c8ac2ef
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-11-17 14:33:24 +0900

    クラス作成

---
+++ SpringBoot/src/test/java/com/qiitabuilder/mapper/UserMapperTest.java
@@ -0,0 +1,168 @@
+package com.qiitabuilder.mapper;
+
+import com.qiitabuilder.domain.Article;
+import com.qiitabuilder.domain.Tag;
+import com.qiitabuilder.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@MybatisTest
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserMapperTest {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    private void beforeEach() {
+        jdbcTemplate.execute("create table users\n" +
+                "(\n" +
+                "   user_id      int auto_increment\n" +
+                "       primary key,\n" +
+                "   uid          text null,\n" +
+                "   photo_url    text null,\n" +
+                "   display_name text null,\n" +
+                "   password     text null\n" +
+                ");\n");
+
+
+        jdbcTemplate.execute("create table articles\n" +
+                "(\n" +
+                "   article_id       int auto_increment\n" +
+                "       primary key,\n" +
+                "   user_id          int          null,\n" +
+                "   created_at       datetime     null,\n" +
+                "   updated_at       datetime     null,\n" +
+                "   title            varchar(100) null,\n" +
+                "   content          text         null,\n" +
+                "   qiita_article_id text         null,\n" +
+                "   state_flag       int          null,\n" +
+                "   constraint fk_articles_userid\n" +
+                "       foreign key (user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table tags\n" +
+                "(\n" +
+                "   tag_id   int auto_increment\n" +
+                "       primary key,\n" +
+                "   tag_name text null\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table articles_tags_relations\n" +
+                "(\n" +
+                "   tags_relation_id int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id       int null,\n" +
+                "   posted_user_id   int null,\n" +
+                "   tag_id           int null,\n" +
+                "   constraint fk_tags_relations_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_tags_relations_tagid\n" +
+                "       foreign key (tag_id) references tags (tag_id),\n" +
+                "   constraint fk_tags_relations_userid\n" +
+                "       foreign key (posted_user_id) references articles (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table feedbacks\n" +
+                "(\n" +
+                "   feedback_id int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id  int      null,\n" +
+                "   user_id     int      null,\n" +
+                "   created_at  datetime null,\n" +
+                "   updated_at  datetime null,\n" +
+                "   content     text     null,\n" +
+                "   delete_flag int      null,\n" +
+                "   constraint fk_feedbacks_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_feedbacks_userid\n" +
+                "       foreign key (user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table my_articles\n" +
+                "(\n" +
+                "   my_article_id    int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id       int null,\n" +
+                "   posted_user_id   int null,\n" +
+                "   register_user_id int null,\n" +
+                "   constraint fk_myarticles_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_myarticles_posted_userid\n" +
+                "       foreign key (posted_user_id) references articles (user_id),\n" +
+                "   constraint fk_myarticles_register_userid\n" +
+                "       foreign key (register_user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table qiita_configurations\n" +
+                "(\n" +
+                "   qiita_cofiguration_id int auto_increment\n" +
+                "       primary key,\n" +
+                "   user_id               int         not null,\n" +
+                "   state                 varchar(36) null,\n" +
+                "   code                  varchar(40) null,\n" +
+                "   token                 varchar(40) null,\n" +
+                "   constraint qiita_configurations_user_id_uindex\n" +
+                "       unique (user_id),\n" +
+                "   constraint qiita_configurations_users_user_id_fk\n" +
+                "       foreign key (user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table qiita_recommends\n" +
+                "(\n" +
+                "   recommend_id      int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id        int null,\n" +
+                "   posted_user_id    int null,\n" +
+                "   recommend_user_id int null,\n" +
+                "   constraint fk_qiitarecommends_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_qiitarecommends_posted_userid\n" +
+                "       foreign key (posted_user_id) references articles (user_id),\n" +
+                "   constraint fk_qiitarecommends_recommend_userid\n" +
+                "       foreign key (recommend_user_id) references users (user_id)\n" +
+                ");\n");
+    }
+
+
+    @AfterEach
+    private void beforeAfter() {
+        jdbcTemplate.execute("DROP TABLE qiita_recommends");
+        jdbcTemplate.execute("DROP TABLE qiita_configurations");
+        jdbcTemplate.execute("DROP TABLE my_articles");
+        jdbcTemplate.execute("DROP TABLE feedbacks");
+        jdbcTemplate.execute("DROP TABLE articles_tags_relations");
+        jdbcTemplate.execute("DROP TABLE tags");
+        jdbcTemplate.execute("DROP TABLE articles");
+        jdbcTemplate.execute("DROP TABLE users");
+    }
+
+    @Test
+    void fetchUserDetails(Integer userId){
+        User user=new User();
+        user.setUserId(1);
+    }
+
+
+}
