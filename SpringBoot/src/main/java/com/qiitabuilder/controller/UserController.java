51f0d23cfd547a2ae2a4dbab1fc74b811536c67a
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-11-17 14:32:44 +0900

    ユーザー詳細画面：ヘッダー作成

--- SpringBoot/src/main/java/com/qiitabuilder/controller/UserController.java
+++ SpringBoot/src/main/java/com/qiitabuilder/controller/UserController.java
@@ -16,18 +16,22 @@ public class UserController {
     private UserService userService;
 
     /**
-     *
      * userを新規登録するメソッド
      *
      * @param user フロント側から投げられてくるリクエストボディ
      * @return
      */
-    @RequestMapping(value = "/" , method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
     public Map<String, String> insertUser(@RequestBody User user) {
 
         userService.insertUser(user);
 
         return null;
+    }
+
+    @RequestMapping(value = "userId", method = RequestMethod.GET)
+    public Integer findUserIdByUid(String uid) {
+        return userService.findUserIdByUid(uid);
     }
 
 }
