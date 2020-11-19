fac70d8827bc432e73a8efdbedc21094e6fb5191
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-11-19 13:35:44 +0900

    ArticleControllerTest:get_ByUserIdテスト作成

--- SpringBoot/src/test/java/com/qiitabuilder/controller/ArticleControllerTest.java
+++ SpringBoot/src/test/java/com/qiitabuilder/controller/ArticleControllerTest.java
@@ -1,10 +1,147 @@
 package com.qiitabuilder.controller;
 
+import com.qiitabuilder.domain.Article;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class ArticleControllerTest {
+    @Autowired
+    private ArticleController articleController;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    private void beforeEach() {
+        jdbcTemplate.execute("create table users\n" +
+                "(\n" +
+                "   user_id      int auto_increment\n" +
+                "       primary key,\n" +
+                "   uid          text null,\n" +
+                "   photo_url    text null,\n" +
+                "   display_name text null,\n" +
+                "   password     text null\n" +
+                ");\n");
+
+
+        jdbcTemplate.execute("create table articles\n" +
+                "(\n" +
+                "   article_id       int auto_increment\n" +
+                "       primary key,\n" +
+                "   user_id          int          null,\n" +
+                "   created_at       datetime     null,\n" +
+                "   updated_at       datetime     null,\n" +
+                "   title            varchar(100) null,\n" +
+                "   content          text         null,\n" +
+                "   qiita_article_id text         null,\n" +
+                "   state_flag       int          null,\n" +
+                "   constraint fk_articles_userid\n" +
+                "       foreign key (user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table tags\n" +
+                "(\n" +
+                "   tag_id   int auto_increment\n" +
+                "       primary key,\n" +
+                "   tag_name text null\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table articles_tags_relations\n" +
+                "(\n" +
+                "   tags_relation_id int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id       int null,\n" +
+                "   posted_user_id   int null,\n" +
+                "   tag_id           int null,\n" +
+                "   constraint fk_tags_relations_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_tags_relations_tagid\n" +
+                "       foreign key (tag_id) references tags (tag_id),\n" +
+                "   constraint fk_tags_relations_userid\n" +
+                "       foreign key (posted_user_id) references articles (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table feedbacks\n" +
+                "(\n" +
+                "   feedback_id int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id  int      null,\n" +
+                "   user_id     int      null,\n" +
+                "   created_at  datetime null,\n" +
+                "   updated_at  datetime null,\n" +
+                "   content     text     null,\n" +
+                "   delete_flag int      null,\n" +
+                "   constraint fk_feedbacks_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_feedbacks_userid\n" +
+                "       foreign key (user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table my_articles\n" +
+                "(\n" +
+                "   my_article_id    int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id       int null,\n" +
+                "   posted_user_id   int null,\n" +
+                "   register_user_id int null,\n" +
+                "   constraint fk_myarticles_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_myarticles_posted_userid\n" +
+                "       foreign key (posted_user_id) references articles (user_id),\n" +
+                "   constraint fk_myarticles_register_userid\n" +
+                "       foreign key (register_user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table qiita_configurations\n" +
+                "(\n" +
+                "   qiita_cofiguration_id int auto_increment\n" +
+                "       primary key,\n" +
+                "   user_id               int         not null,\n" +
+                "   state                 varchar(36) null,\n" +
+                "   code                  varchar(40) null,\n" +
+                "   token                 varchar(40) null,\n" +
+                "   constraint qiita_configurations_user_id_uindex\n" +
+                "       unique (user_id),\n" +
+                "   constraint qiita_configurations_users_user_id_fk\n" +
+                "       foreign key (user_id) references users (user_id)\n" +
+                ");\n");
+
+        jdbcTemplate.execute("create table qiita_recommends\n" +
+                "(\n" +
+                "   recommend_id      int auto_increment\n" +
+                "       primary key,\n" +
+                "   article_id        int null,\n" +
+                "   posted_user_id    int null,\n" +
+                "   recommend_user_id int null,\n" +
+                "   constraint fk_qiitarecommends_articleid\n" +
+                "       foreign key (article_id) references articles (article_id),\n" +
+                "   constraint fk_qiitarecommends_posted_userid\n" +
+                "       foreign key (posted_user_id) references articles (user_id),\n" +
+                "   constraint fk_qiitarecommends_recommend_userid\n" +
+                "       foreign key (recommend_user_id) references users (user_id)\n" +
+                ");\n");
+    }
+
+
+    @AfterEach
+    private void beforeAfter() {
+        jdbcTemplate.execute("DROP TABLE qiita_recommends");
+        jdbcTemplate.execute("DROP TABLE qiita_configurations");
+        jdbcTemplate.execute("DROP TABLE my_articles");
+        jdbcTemplate.execute("DROP TABLE feedbacks");
+        jdbcTemplate.execute("DROP TABLE articles_tags_relations");
+        jdbcTemplate.execute("DROP TABLE tags");
+        jdbcTemplate.execute("DROP TABLE articles");
+        jdbcTemplate.execute("DROP TABLE users");
+    }
 
     @Test
     void searchArticles() {
@@ -27,10 +164,131 @@ class ArticleControllerTest {
     }
 
     @Test
-    void getFeedbackedArticlesByUserId() {
+    void getFeedbackedArticlesByUserId_nullチェック() {
+        String[] userSqlArr = CollectionSQL.insertUsers.split("\n", 0);
+        String[] articleSqlArr = CollectionSQL.insertArticles.split("\n", 0);
+        String[] feedbackSqlArr = CollectionSQL.insertFeedbacks.split("\n", 0);
+        String[] qiitaRecommendSqlArr = CollectionSQL.insertQiitaRecommends.split("\n", 0);
+        String[] tagSqlArr = CollectionSQL.insertTags.split("\n", 0);
+        String[] tagRelationSqlArr = CollectionSQL.insertArticlesTagsRelations.split("\n", 0);
+        String[] myArticleSqlArr = CollectionSQL.insertMyArticles.split("\n", 0);
+
+        Arrays.stream(userSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(articleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(feedbackSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(qiitaRecommendSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagRelationSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(myArticleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+
+        List<Article> articleList=articleController.getFeedbackedArticlesByUserId(null);
+        assertNull(articleList);
+    }
+    @Test
+    void getFeedbackedArticlesByUserId_notNullチェック() {
+        String[] userSqlArr = CollectionSQL.insertUsers.split("\n", 0);
+        String[] articleSqlArr = CollectionSQL.insertArticles.split("\n", 0);
+        String[] feedbackSqlArr = CollectionSQL.insertFeedbacks.split("\n", 0);
+        String[] qiitaRecommendSqlArr = CollectionSQL.insertQiitaRecommends.split("\n", 0);
+        String[] tagSqlArr = CollectionSQL.insertTags.split("\n", 0);
+        String[] tagRelationSqlArr = CollectionSQL.insertArticlesTagsRelations.split("\n", 0);
+        String[] myArticleSqlArr = CollectionSQL.insertMyArticles.split("\n", 0);
+
+        Arrays.stream(userSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(articleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(feedbackSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(qiitaRecommendSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagRelationSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(myArticleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+
+        List<Article> articleList=articleController.getFeedbackedArticlesByUserId(1);
+        assertNotNull(articleList);
     }
 
     @Test
-    void getMyArticlesByUserId() {
+    void getMyArticlesByUserId_nullチェック() {
+        String[] userSqlArr = CollectionSQL.insertUsers.split("\n", 0);
+        String[] articleSqlArr = CollectionSQL.insertArticles.split("\n", 0);
+        String[] feedbackSqlArr = CollectionSQL.insertFeedbacks.split("\n", 0);
+        String[] qiitaRecommendSqlArr = CollectionSQL.insertQiitaRecommends.split("\n", 0);
+        String[] tagSqlArr = CollectionSQL.insertTags.split("\n", 0);
+        String[] tagRelationSqlArr = CollectionSQL.insertArticlesTagsRelations.split("\n", 0);
+        String[] myArticleSqlArr = CollectionSQL.insertMyArticles.split("\n", 0);
+
+        Arrays.stream(userSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(articleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(feedbackSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(qiitaRecommendSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagRelationSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(myArticleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+
+        List<Article> articleList=articleController.getMyArticlesByUserId(null);
+        assertNull(articleList);
+    }
+    @Test
+    void getMyArticlesByUserId_notNullチェック() {
+        String[] userSqlArr = CollectionSQL.insertUsers.split("\n", 0);
+        String[] articleSqlArr = CollectionSQL.insertArticles.split("\n", 0);
+        String[] feedbackSqlArr = CollectionSQL.insertFeedbacks.split("\n", 0);
+        String[] qiitaRecommendSqlArr = CollectionSQL.insertQiitaRecommends.split("\n", 0);
+        String[] tagSqlArr = CollectionSQL.insertTags.split("\n", 0);
+        String[] tagRelationSqlArr = CollectionSQL.insertArticlesTagsRelations.split("\n", 0);
+        String[] myArticleSqlArr = CollectionSQL.insertMyArticles.split("\n", 0);
+
+        Arrays.stream(userSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(articleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(feedbackSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(qiitaRecommendSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagRelationSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(myArticleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+
+        List<Article> articleList=articleController.getMyArticlesByUserId(1);
+        assertNotNull(articleList);
+    }
+
+    @Test
+    void  getArticlesByUserId_nullチェック(){
+        String[] userSqlArr = CollectionSQL.insertUsers.split("\n", 0);
+        String[] articleSqlArr = CollectionSQL.insertArticles.split("\n", 0);
+        String[] feedbackSqlArr = CollectionSQL.insertFeedbacks.split("\n", 0);
+        String[] qiitaRecommendSqlArr = CollectionSQL.insertQiitaRecommends.split("\n", 0);
+        String[] tagSqlArr = CollectionSQL.insertTags.split("\n", 0);
+        String[] tagRelationSqlArr = CollectionSQL.insertArticlesTagsRelations.split("\n", 0);
+        String[] myArticleSqlArr = CollectionSQL.insertMyArticles.split("\n", 0);
+
+        Arrays.stream(userSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(articleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(feedbackSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(qiitaRecommendSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagRelationSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(myArticleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+
+        List<Article> articleList=articleController.getArticlesByUserId(null);
+        assertNull(articleList);
+    }
+    @Test
+    void  getArticlesByUserId_notNullチェック(){
+        String[] userSqlArr = CollectionSQL.insertUsers.split("\n", 0);
+        String[] articleSqlArr = CollectionSQL.insertArticles.split("\n", 0);
+        String[] feedbackSqlArr = CollectionSQL.insertFeedbacks.split("\n", 0);
+        String[] qiitaRecommendSqlArr = CollectionSQL.insertQiitaRecommends.split("\n", 0);
+        String[] tagSqlArr = CollectionSQL.insertTags.split("\n", 0);
+        String[] tagRelationSqlArr = CollectionSQL.insertArticlesTagsRelations.split("\n", 0);
+        String[] myArticleSqlArr = CollectionSQL.insertMyArticles.split("\n", 0);
+
+        Arrays.stream(userSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(articleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(feedbackSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(qiitaRecommendSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(tagRelationSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+        Arrays.stream(myArticleSqlArr).forEach((sql) -> jdbcTemplate.execute(sql));
+
+        List<Article> articleList=articleController.getArticlesByUserId(1);
+        assertNotNull(articleList);
     }
 }
