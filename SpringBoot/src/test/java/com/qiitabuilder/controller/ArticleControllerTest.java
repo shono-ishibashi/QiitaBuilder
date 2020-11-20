14ada2d31ba604a5be49757153ada0532d9a3beb
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-20 11:14:50 +0900

    テストコードの修正

--- SpringBoot/src/test/java/com/qiitabuilder/controller/ArticleControllerTest.java
+++ SpringBoot/src/test/java/com/qiitabuilder/controller/ArticleControllerTest.java
@@ -22,6 +22,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 @SpringBootTest
@@ -58,28 +59,67 @@ class ArticleControllerTest {
     }
 
     @Test
-    void postArticle() throws Exception {
+    void postArticle_正常系() throws Exception {
         List<Tag> tags = new ArrayList<>(Arrays.asList(new Tag(1, "tag1", null), new Tag(2, "tag2", null)));
 
 
         Article article = Article.builder()
                 .articleId(null)
-                .title("")
+                .title("title_test")
                 .tags(tags)
                 .content("content_test")
                 .build();
 
-        doReturn(article).when(articleService).saveArticle(article);
+        Article expectBody = Article.builder()
+                .articleId(1)
+                .title("title_test")
+                .tags(tags)
+                .content("content_test")
+                .build();
+
+
+        doReturn(expectBody).when(articleService).saveArticle(article);
 
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(article);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/article/")
+        mockMvc.perform(MockMvcRequestBuilders.post("/article/")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(SecurityMockMvcRequestPostProcessors.user(createLoginUser())))
+                .andExpect(content().json(mapper.writeValueAsString(expectBody)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+    @Test
+    void postArticle_異常系_tagが0件の場合() throws Exception {
+        List<Tag> tags = new ArrayList<>();
+
+
+        Article article = Article.builder()
+                .articleId(null)
+                .title("title_test")
+                .tags(tags)
+                .content("content_test")
+                .build();
+
+        Article expectBody = Article.builder()
+                .articleId(1)
+                .title("title_test")
+                .tags(tags)
+                .content("content_test")
+                .build();
+
+
+        doReturn(expectBody).when(articleService).saveArticle(article);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(article);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/article/")
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
                 .with(SecurityMockMvcRequestPostProcessors.user(createLoginUser())))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
-
     }
 
     @Test
