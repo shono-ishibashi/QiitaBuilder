376c65facb84101774cfbd9a20169b840f20053e
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-20 14:15:46 +0900

    テスト

--- SpringBoot/src/test/java/com/qiitabuilder/controller/UserControllerTest.java
+++ SpringBoot/src/test/java/com/qiitabuilder/controller/UserControllerTest.java
@@ -1,12 +1,64 @@
 package com.qiitabuilder.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qiitabuilder.domain.User;
+import com.qiitabuilder.security.SimpleLoginUser;
+import com.qiitabuilder.service.ArticleService;
+import com.qiitabuilder.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Service層をモックする
+    @MockBean
+    private UserService userService;
+
+    private SimpleLoginUser createLoginUser() {
+        User user = new User();
+        user.setUserId(1);
+        user.setUid("uid");
+        user.setPassword("password");
+
+        doNothing().when(userService).insertUser(user);
+
+        return new SimpleLoginUser(user);
+    }
+
     @Test
-    void insertUser() {
+    void insertUser() throws Exception {
+        User user = new User();
+        user.setUserId(1);
+        user.setUid("uid");
+        user.setPassword("password");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    void findUserIdByUid(){
+
     }
 }
