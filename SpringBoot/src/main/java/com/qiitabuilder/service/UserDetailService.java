8fd4988d8b6f6ed44b1bd7a32bd44a32fd1df083
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-11-24 12:18:40 +0900

    テストの実装

--- SpringBoot/src/main/java/com/qiitabuilder/service/UserDetailService.java
+++ SpringBoot/src/main/java/com/qiitabuilder/service/UserDetailService.java
@@ -18,17 +18,18 @@ public class UserDetailService {
 
     /**
      * ユーザー名、写真URL、Qiita投稿済み記事件数、フィードバック数、総Qiita投稿推薦数、タグ名と使用回数を持ったタグリスト
-     *　上記を持ったユーザーを取得するメソッド
-     * @param userId　
+     * 　上記を持ったユーザーを取得するメソッド
+     *
+     * @param userId
      * @return　取得したいユーザー情報
      */
     public User fetchUserDetails(Integer userId) {
-        User user=userMapper.fetchUserDetails(userId);
+        User user = userMapper.fetchUserDetails(userId);
         if (Objects.isNull(user)) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         SimpleLoginUser loginUser = (SimpleLoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (user.getUid().equals(loginUser.getUser().getUid())){
+        if (user.getUid().equals(loginUser.getUser().getUid())) {
             user.setIsLoginUser(true);
-        }else {
+        } else {
             user.setIsLoginUser(false);
         }
         return user;
