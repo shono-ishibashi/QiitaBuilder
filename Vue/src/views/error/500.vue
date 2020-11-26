d5680dbdc9ba9c750bcf44849185056cee007d1c
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-26 15:50:58 +0900

    500エラーページの作成

---
+++ Vue/src/views/error/500.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <h1>500 Internal Server Error</h1>
+    <p>
+      サーバーエラーが発生しました。
+    </p>
+    <p>
+      サーバーの問題でお探しのページを表示できません。
+    </p>
+    <p>
+      再度時間をおいてアクセスしてください。
+    </p>
+    <router-link :to="{ name: 'articleList' }">
+      QiitaBuilderトップページへ
+    </router-link>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "500"
+}
+</script>
