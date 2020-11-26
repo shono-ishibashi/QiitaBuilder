d78bfb2bceb29e472d555494d0e57c5e54e9d896
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-26 13:03:19 +0900

    ・ユーザー名の前に@を追記 ・ユーザー名とQiita連携ボタンの位置を上部に変更 ・count情報の数を目立たせるようCSS追加

--- Vue/src/views/UserDetail.vue
+++ Vue/src/views/UserDetail.vue
@@ -3,6 +3,15 @@
     <v-row>
       <v-col cols="12" sm="12" md="6">
         <v-row>
+          <v-col cols="6" class="contentWrap"><span
+              style="font-weight: bold; font-size: x-large;">@{{ userDetail.displayName }}</span></v-col>
+          <v-col cols="6" class="contentWrap">
+            <v-btn @click="toQiitaAPIAuthentication" v-if="userDetail.isLoginUser" color="#5bc8ac" elevation="2"
+                   style="font-weight: bold" dark>Qiita連携
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
           <v-col cols=6 class="contentWrap">
             <v-avatar size="140">
               <img :src="userDetail.photoUrl" alt=""/></v-avatar>
@@ -22,40 +31,31 @@
             </v-alert>
           </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="6" style="font-size: large" class="contentWrap"><span
-              style="font-weight: bold">{{ userDetail.displayName }}</span></v-col>
-          <v-col cols="6" class="contentWrap">
-            <v-btn @click="toQiitaAPIAuthentication" v-if="userDetail.isLoginUser" color="#5bc8ac" elevation="2"
-                   style="font-weight: bold" dark>Qiita連携
-            </v-btn>
+
+        <v-row align-content="center" justify="center" class="box16">
+          <v-col cols="4" class="box6">
+            <v-row align-content="center" justify="center">
+              Qiita投稿数 / Builder投稿数
+            </v-row>
+            <v-row align-content="center" justify="center" class="count">
+              {{ userDetail.postedArticleCount }} / {{ notDraftArticles.length }}
+            </v-row>
           </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols=5>
-            <v-card>
-              <v-layout justify-center>
-                Qiita投稿数 / Builder投稿数<br></v-layout>
-              <v-layout justify-center>
-                {{ userDetail.postedArticleCount }} / {{ notDraftArticles.length }}
-              </v-layout>
-            </v-card>
+          <v-col cols="3" class="box6">
+            <v-row align-content="center" justify="center">
+              FB数
+            </v-row>
+            <v-row align-content="center" justify="center" class="count">
+              {{ userDetail.feedbackCount }}
+            </v-row>
           </v-col>
-          <v-col cols=3>
-            <v-card>
-              <v-layout justify-center>FB数</v-layout>
-              <v-layout justify-center>
-                {{ userDetail.feedbackCount }}
-              </v-layout>
-            </v-card>
-          </v-col>
-          <v-col cols="4">
-            <v-card>
-              <v-layout justify-center>総獲得推奨数</v-layout>
-              <v-layout justify-center>
-                {{ userDetail.qiitaRecommendedAllCount }}
-              </v-layout>
-            </v-card>
+          <v-col cols="3" class="box6">
+            <v-row align-content="center" justify="center">
+              総獲得推奨数
+            </v-row>
+            <v-row align-content="center" justify="center" class="count">
+              {{ userDetail.qiitaRecommendedAllCount }}
+            </v-row>
           </v-col>
         </v-row>
       </v-col>
@@ -514,5 +514,25 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.count {
+  font-size: x-large;
+  font-weight: bold;
+}
+
+.box16 {
+  padding: 0.5em 1em;
+  margin: 2em 0;
+  background: -webkit-repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px, #e9f4ff 3px, #e9f4ff 7px);
+  background: repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px, #e9f4ff 3px, #e9f4ff 7px);
+}
+
+.box6 {
+  padding: 0.5em 1em;
+  margin: 2em 0.25em;
+  background: #f0f7ff;
+  border: dashed 2px #5bc8ac; /*点線*/
+  border-radius: 10px;
 }
 </style>
