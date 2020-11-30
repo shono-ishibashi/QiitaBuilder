4b6d28ed11b936678eb86c11a0c15f173d217ade
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-11-30 11:38:09 +0900

    setUpForTest

--- Vue/__tests__/views/Ranking.test.js
+++ Vue/__tests__/views/Ranking.test.js
@@ -1,15 +1,67 @@
-import { shallowMount } from '@vue/test-utils';
-import Component from '@/components/auth/GoogleLogin';
+import {shallowMount, createLocalVue} from '@vue/test-utils';
+import Vuex from 'vuex';
+import Component from '@/views/Ranking';
 
-let wrapper
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+let wrapper;
+let store;
+let actions;
+let mutations;
+let state;
+let getters;
 
 beforeEach(() => {
-    wrapper = shallowMount(Component)
+
+    actions = {
+        fetchRankingUser: jest.fn()
+    };
+    mutations = {};
+    state = {
+        rankingUsers: []
+    };
+    getters = {
+        users(state) {
+            return state.rankingUsers;
+        },
+        relationArticles(state) {
+            return state.rankingUsers;
+        }
+    };
+
+    store = new Vuex.Store({
+        modules: {
+            users: {
+                namespaced: true,
+                state,
+                mutations,
+                actions,
+                getters
+            },
+            auth: {
+                state: {
+                    apiToken: 'token'
+                },
+                getters: {
+                    apiToken: jest.fn()
+                }
+            }
+        }
+    })
+
+    wrapper = shallowMount(Component, {
+        store,
+        localVue
+    })
+
 })
 
 afterEach(() => {
     wrapper.destroy();
 })
+
+
 describe('Testing App component', () => {
 
     it('is a Vue instance', () => {
