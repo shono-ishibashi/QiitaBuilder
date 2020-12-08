e16568635fbc122d748f0256fdfa69522f9d540e
Morita Soshi <66908774+moritasoshi@users.noreply.github.com>
2020-12-08 11:57:28 +0900

    不要なログイン画面への遷移を削除

--- Vue/src/router/index.js
+++ Vue/src/router/index.js
@@ -91,8 +91,7 @@ router.beforeResolve(async (to, from, next) => {
                 }
                 //ログイン中でないならログアウト処理を行う
             } else {
-                // await store.dispatch('auth/logout');
-                await next({path: '/login'})
+                await store.dispatch('auth/logout');
             }
         })
     }
