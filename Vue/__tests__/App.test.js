ca1629730c0356573220a3b15197b2bb46f7c857
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-12-09 16:33:27 +0900

    テスト準備

--- Vue/__tests__/App.test.js
+++ Vue/__tests__/App.test.js
@@ -1,18 +1,49 @@
-import { shallowMount } from '@vue/test-utils';
-import Component from '@/components/auth/GoogleLogin';
+import {shallowMount, createLocalVue} from '@vue/test-utils';
+import VueRouter from "vue-router";
+import Vuex from 'vuex';
+import Component from '@/App';
+import {afterEach, beforeEach, describe, jest} from "@jest/globals";
 
-let wrapper
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+localVue.use(Vuex);
+
+const router = new VueRouter();
+
+let wrapper;
+let store;
+let isNotFoundMock = jest.fn();
+let isInternalServerErrorMock = jest.fn();
 
 beforeEach(() => {
-  wrapper = shallowMount(Component)
+    isNotFoundMock.mockReturnValue(false);
+    isInternalServerErrorMock.mockReturnValue(false);
+
+    store = new Vuex.Store({
+        modules: {
+            windows: {
+                namespaced: true,
+                getters: {
+                    isNotFound: isNotFoundMock,
+                    isInternalServerError: isInternalServerErrorMock
+                }
+            }
+        },
+    })
+
+    wrapper = shallowMount(Component, {
+        localVue,
+        router,
+        store
+    })
 })
 
 afterEach(() => {
-  wrapper.destroy();
+    wrapper.destroy();
 })
 describe('Testing App component', () => {
 
-  it('is a Vue instance', () => {
-    expect(wrapper.isVueInstance).toBeTruthy();
-  })
+    test('is a Vue instance', () => {
+        expect(wrapper.isVueInstance).toBeTruthy();
+    })
 })
